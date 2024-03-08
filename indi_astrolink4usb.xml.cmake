d7d84bd0a1e420575bb26c690a8310e91af9cf92
astrojolo <astrojolo@astrojolo.com>
2024-03-08 09:02:14 +0100

    Manufacturer added

--- indi_astrolink4usb.xml.cmake
+++ indi_astrolink4usb.xml.cmake
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <driversList>
 <devGroup group="Auxiliary">
-        <device label="AstroLink 4 USB">
+        <device label="AstroLink 4 USB" manufacturer="Astrojolo">
                 <driver name="AstroLink4 USB">indi_astrolink4usb</driver>
                 <version>@ASTROLINK4_VERSION_MAJOR@.@ASTROLINK4_VERSION_MINOR@</version>
         </device>
